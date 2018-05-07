8a6a80070c77fcad8bc70ff9433432048f909feb
tamos <tyler.amos@alumni.carleton.ca>
2018-05-06 22:19:29 -0500

    1019 latest

--- paper_code/flee-release/run_test_sim.py
+++ paper_code/flee-release/run_test_sim.py
@@ -1,5 +1,12 @@
 from InputGeography import InputGeography
-from flee.flee import Person, Location, Ecosystem
+from flee.flee import Ecosystem
+
+# plotting imports
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+
 
 def go():
 	geog = InputGeography()
@@ -27,19 +34,60 @@ def go():
 
 	import numpy as np 
 	lm_key = list(lm.keys())
+
+	import pandas as pd
+	res_list = {}
+	for i in lm_key:
+		if i not in res_list:
+			res_list[i] = []
+
 	for each_step in range(0,end_time):
+
+		# this should be a list of locations, we add one to each
+		# each agent will be a HH, based on outflow nums from IOM
 		for each_agent in range(0, 100):
 			place = np.random.randint(0, len(lm_key))
 			e.addAgent(location=lm[lm_key[place]])
-
 
 		e.evolve()
 
 		e.printInfo()
 		print("\n---------")
-		print("\n\nTIME IS: {}".format(each_step))
+		print("\nTIME IS: {}".format(each_step))
+
+		loc_list = []
 		for each_location, loc_obj in lm.items():
-			print(each_location, loc_obj.numAgents)
+			res_list[each_location].append(loc_obj.numAgents)
+
+	results = pd.DataFrame(res_list)
+	#results.columns = lm_key
+
+	results.to_csv('simulation_results.csv')
+	from plotter import read_datafile
+
+	data = read_datafile("simulation_results.csv")
+
+	x = data[:,0] # time
+	y = data[:,1] # camp 22 (Mahama)
+	z = data[:,2] # third column for comparison
+
+	fig = plt.figure()
+	ax1 = fig.add_subplot(111)
+
+	ax1.set_title("Title")
+	ax1.set_xlabel('Time')
+	ax1.set_ylabel('No. of Refugees')
+
+	ax1.plot(x,y, c='r')
+	ax1.plot(x,z, c='b')
+
+	leg = ax1.legend()
+
+	plt.savefig('simres.png')
+
+
+
+	# need to compare with some loss function
 
 
 if __name__ == "__main__":
